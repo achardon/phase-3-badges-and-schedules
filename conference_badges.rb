a25c96d4121f8a63a30e157d513f462ccb95cbfe
Alessandra Chardon <achardon@ucla.edu>
2021-12-02 15:50:48 -0500

    commit tree 3a409294545b451fb4ee4656d4d059aa06c39fe3

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map {|p| "Hello, my name is #{p}."}
+end
+
+def assign_rooms(array)
+    room = 0
+    rooms_array = array.map do |p|
+        room += 1
+        "Hello, #{p}! You'll be assigned to room #{room}!" 
+    end
+end
+
+def printer(array)
+    batch_badge_creator(array).each {|p| puts p}
+    assign_rooms(array).each {|p| puts p}
+end
